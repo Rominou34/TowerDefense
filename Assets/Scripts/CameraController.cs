b6eec5a632161c181425823c6da0c1f60ef3ae6f
Romain Arnaud <arnaud.romain34@gmail.com>
2019-01-06 19:39:19 +0100

    Update camera work and add missing folders
    - Finished the tutorial EP 07, with the finished camera work (will have
      to be polished later)
      - Added all the other folders (will maybe clean that later, for now I
        add everything)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private bool canMove = true;
 
     public float panSpeed = 10f;
     public float panBorderThickness = 10f;
+
+    public float scrollSpeed = 500f;
+    public float minY = 5f;
+    public float maxY = 80;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +22,17 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            canMove = !canMove;
+        }
+
+        if(!canMove)
+        {
+            return;
+        }
+
+        // Moving the camera (WASD or mouse at the border of the screen)
         if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
@@ -25,13 +41,22 @@ public class CameraController : MonoBehaviour
         {
             transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
         }
-        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
         {
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.Self);
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
         }
-        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x <= panBorderThickness)
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.Self);
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
         }
+
+        // Scrolling for zoom
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 position = transform.position;
+        position.y -= scroll * scrollSpeed * Time.deltaTime;
+
+        // Min and max height
+        position.y = Mathf.Clamp(position.y, minY, maxY);
+        transform.position = position;
     }
 }
